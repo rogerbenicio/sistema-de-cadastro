2b556312e1cb08df768540c364f4bf371400083a
Rogerio <rogerbenicio@hotmail.com>
2022-07-23 18:40:21 -0300

    adicionando console log

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação esta se conectando ao banco de dados")
+// mais console log tetste
+console.log("Alteração feita na minha máquina")
+console.log("Enviando para o GitHub")
